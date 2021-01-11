95bd0e75683ce9ce0b96d51aa4dfa17084bfc189
Vitali Rapalis <vitali.rapalis@smawandi.com>
2021-01-11 15:03:17 +0100

    set up backend gradle

--- backend/apps/entryou/uaa/src/main/java/com/vrapalis/www/entryou/uaa/UaaApplication.java
+++ backend/apps/entryou/uaa/src/main/java/com/vrapalis/www/entryou/uaa/UaaApplication.java
@@ -2,6 +2,10 @@ package com.vrapalis.www.entryou.uaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class UaaApplication {
@@ -11,3 +15,14 @@ public class UaaApplication {
     }
 
 }
+
+@RestController
+@RequestMapping("api/greeting")
+class GreetingRestEndpoint {
+    @GetMapping
+    public ResponseEntity<String> greeting() {
+        return ResponseEntity.ok("Hallo World!!!");
+    }
+}
+
+
