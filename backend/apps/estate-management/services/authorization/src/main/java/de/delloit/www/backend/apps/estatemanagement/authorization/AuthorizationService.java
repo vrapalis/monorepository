87a5d3a7dbe507fa0bd29bb0de7fff60b7ca9c42
Vitali Rapalis <vitali.rapalis@smawandi.com>
2021-08-11 18:23:49 +0200

    added spring config server

--- backend/apps/estate-management/services/authorization/src/main/java/de/delloit/www/backend/apps/estatemanagement/authorization/AuthorizationService.java
+++ backend/apps/estate-management/services/authorization/src/main/java/de/delloit/www/backend/apps/estatemanagement/authorization/AuthorizationService.java
@@ -1,13 +1,33 @@
 package de.delloit.www.backend.apps.estatemanagement.authorization;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthorizationService {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationService.class, args);
     }
 
+}
+
+@RefreshScope
+@RestController
+class ConfigRestController {
+
+    @Value("${my.prop}")
+    private String myProp;
+
+    @GetMapping("/my-prop")
+    public ResponseEntity<String> getMyProp() {
+        return ResponseEntity.ok(myProp);
+    }
 }
---
+++ backend/apps/estate-management/services/config/src/main/java/com/delloit/www/backend/apps/estatemanagement/config/ConfigService.java
@@ -0,0 +1,17 @@
+package com.delloit.www.backend.apps.estatemanagement.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigService.class, args);
+    }
+
+}
