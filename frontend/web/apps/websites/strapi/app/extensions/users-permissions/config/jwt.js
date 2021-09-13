28221fa695e5c92687298f62bf169f8b56185dfd
Ubuntu <ubuntu@ip-172-31-5-9.eu-central-1.compute.internal>
2021-09-13 07:56:26 +0000

    clean

--- frontend/web/apps/websites/strapi/app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'daf9d450-a2a2-4ba1-8778-eac9588d54ae'
-};
--- frontend/web/apps/websites/vrapalis/strapi/app/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '9e520e5f-8d7c-4f5a-9f90-1fa674de9a6e'
-};
