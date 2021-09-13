6c6a41203342ed5f62066ec74ff9ea1046e77247
Ubuntu <ubuntu@ip-172-31-5-9.eu-central-1.compute.internal>
2021-09-13 08:09:46 +0000

    set up strapi and db

---
+++ frontend/web/apps/websites/vrapalis/strapi/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'acd3e81ca3047aa3df0a29d93314594b'),
+    },
+  },
+});
---
+++ frontend/web/apps/websites/vrapalis/strapi/app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'de2bd113-d51f-4f18-bacc-82735a8900fc'
+};
