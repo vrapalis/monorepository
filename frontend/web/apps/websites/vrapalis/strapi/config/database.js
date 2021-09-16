1527362eec93e9500785c87c9cdd139231742616
vrapalis <vitali.rapalis@mail.ru>
2021-09-16 15:29:05 +0200

    restructure strapi

---
+++ frontend/web/apps/websites/vrapalis/strapi/config/database.js
@@ -0,0 +1,18 @@
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'postgres',
+        host: env('DATABASE_HOST', 'vrapalis-db.ddns.net'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'vrapalis_website_db'),
+        username: env('DATABASE_USERNAME', 'vrapalis'),
+        password: env('DATABASE_PASSWORD', 'vrapalis123456'),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
+      options: {}
+    },
+  },
+});
---
+++ frontend/web/apps/websites/vrapalis/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ebad5feb-8e8d-4b61-8eae-2c3e4b2a7c0f'
+};
