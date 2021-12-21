966b27a00e8a65e8441c103c56fa1d9b39b8e070
vrapalis <vitali.rapalis@mail.ru>
2021-12-21 09:09:52 +0100

    fix oidc user to persist while oauth2 flow

--- backend/libs/shared/oauth2-server/src/main/java/com/vrapalis/www/backend/libs/shared/oauth2/server/domain/user/service/OAuth2CustomOidcUserServiceImp.java
+++ backend/libs/shared/oauth2-server/src/main/java/com/vrapalis/www/backend/libs/shared/oauth2/server/domain/user/service/OAuth2CustomOidcUserServiceImp.java
@@ -1,18 +1,26 @@
 package com.vrapalis.www.backend.libs.shared.oauth2.server.domain.user.service;
 
+import com.vrapalis.www.backend.libs.shared.oauth2.server.domain.user.entity.OAuth2UserEntity;
+import com.vrapalis.www.backend.libs.shared.oauth2.server.domain.user.model.OAuth2UserModel;
+import lombok.AllArgsConstructor;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
 import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class OAuth2CustomOidcUserServiceImp extends OidcUserService {
+
+    private OAuth2UserService userService;
 
     @Override
     public OidcUser loadUser(OidcUserRequest userRequest) throws OAuth2AuthenticationException {
         final var oidcUser = super.loadUser(userRequest);
-        System.out.println("OIDCUSER: " + oidcUser);
-        return oidcUser;
+        final var auth2UserModel = new OAuth2UserModel(oidcUser);
+        OAuth2UserEntity persistedUser = userService.save(auth2UserModel, userRequest.getClientRegistration().getClientName());
+        return (OidcUser) new OAuth2UserModel(oidcUser, persistedUser);
     }
 }
